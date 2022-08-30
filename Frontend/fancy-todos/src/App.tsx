c45068a5d0b986ce46342c39f2722104020c5f1d
StefanPollin <stefan.pollin@gmx.at>
2022-08-30 20:33:52 +0200

    added cors config to backend, added axios to fetch data from frontend

--- Frontend/fancy-todos/src/App.tsx
+++ Frontend/fancy-todos/src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:5001/api/todos').then((response) => {
+      console.log(response);
+      console.log(todos);
+      setTodos(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <ul>
+          {todos.map((todo: any) => {
+            return <li key={todo.id}>{todo.title}</li>;
+          })}
+        </ul>
       </header>
     </div>
   );
